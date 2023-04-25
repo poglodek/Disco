c62d67b929c59572776ba271ed02630148cf962c
poglodek <pawelogll@gmail.com>
2023-04-25 19:23:51 +0200

    adding logic to handler

--- Disco.Service.Discounts.Application/Commands/Handlers/AddDiscountHandler.cs
+++ Disco.Service.Discounts.Application/Commands/Handlers/AddDiscountHandler.cs
@@ -1,11 +1,25 @@
+using Disco.Service.Discounts.Core.Entities;
+using Disco.Service.Discounts.Core.Repositories;
 using MediatR;
 
 namespace Disco.Service.Discounts.Application.Commands.Handlers;
 
 public class AddDiscountHandler : IRequestHandler<AddDiscount,Unit>
 {
-    public Task<Unit> Handle(AddDiscount request, CancellationToken cancellationToken)
+    private readonly IDiscountRepository _repository;
+
+    public AddDiscountHandler(IDiscountRepository repository)
     {
-        throw new NotImplementedException();
+        _repository = repository;
+    }
+    
+    public async Task<Unit> Handle(AddDiscount request, CancellationToken cancellationToken)
+    {
+        var discount = Discount.Create(Guid.NewGuid(), request.ComapnyId, request.Percent, request.Points, request.StatedDate,
+            request.EndingDate, request.Name);
+
+        await _repository.AddAsync(discount);
+        
+        return Unit.Value;
     }
 }
